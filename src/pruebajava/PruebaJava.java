edda08313bc70c3dfa3adfe2ad4edd289d54b763
Rodrigo1103 <rodrigo.alonso110389@gmail.com>
2023-09-10 11:50:15 -0300

    Test

--- src/pruebajava/PruebaJava.java
+++ src/pruebajava/PruebaJava.java
@@ -4,6 +4,8 @@
  */
 package pruebajava;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author rodri
@@ -16,6 +18,12 @@ public class PruebaJava {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Mi primer programa");
+        ArrayList<Integer> enteros=new ArrayList<>();
+        enteros.add(10);
+        enteros.add(12);
+        for (Integer entero : enteros) {
+            System.out.println(entero);
+        }
     }
     
 }
